0c5e680578ef876f23eada46304e4c73da6d2b7d
ixnp <enprocopios@gmail.com>
2018-04-10 13:21:12 -0700

    PUT with no token will return a 401 test passed

--- test/jb.test.js
+++ test/jb.test.js
@@ -39,5 +39,40 @@ describe('handel token less request', () =>{
       done();
     });
   });
+  //I'm not sure if this is posting as the user isn't coming back with an id
+  //getting a 404
+  test('send 401 for Put request if no token was provided', (done) => {
+    
+    let newUser = getUserParams();
+    
+    superagent.post(SERVER_URL + '/api/game')
+      .set('content-Type', 'application/json')
+      .send(JSON.stringify(newUser))
+      .end((err, res) => {
+        let postId = res.body._id;
+        let updatedPost = {
+          name:'poe'
+        };
+        console.log('POSTID', res.body._id)
+        console.log('newUser', newUser)
+        superagent.put(SERVER_URL + '/api/game?id=' + postId)
+          .set('Content-Type', 'application/json')
+          .send(JSON.stringify(updatedPost))
+          .end((err, res) =>{
+            expect(res.status).toBe(401);
+            done();
+          });
+      });
+  })
 });
 
+// describe('handel valid authorization', () =>{
+//   test('sends 200 for authroized GET request made with a valid id', (done) =>{
+//       let newUser = getUserParams();
+//       let userId;
+//       let token;
+//       let postId;
+//       superagent.post()
+    
+//   })
+// })
